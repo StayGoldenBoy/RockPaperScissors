fbc9aac9cd1f6dbcc373a2be4af50148a1ee3fcd
StaygoldenBoy <staygoldenboy@protonmail.com>
2022-10-07 13:38:40 -0300

    Add js and html file with game.

---
+++ core.js
@@ -0,0 +1,66 @@
+//Since there isn't a better way to select a random item from
+//an array, we will use the Math.random
+
+//First lets create an array of our strings
+let computerChoice = ['Rock', 'Scissors', 'Paper'];
+let randomSelect;
+//Now we define getComputerChoice as a function that pulls
+//from the array
+
+function getComputerChoice(){
+    let randomSelect = computerChoice[Math.floor((Math.random()) * computerChoice.length)];
+    randomSelect = randomSelect.toLowerCase();
+    console.log(randomSelect);
+    return randomSelect;
+    }
+
+    //creating test funciton for randome playerselection
+function getPlayerChoice(){
+    let randomPlayerSelect = computerChoice[Math.floor((Math.random()) * computerChoice.length)];
+    randomPlayerSelect = randomPlayerSelect.toLowerCase();
+    console.log(randomPlayerSelect);
+    return randomPlayerSelect;
+    }
+
+    // Time to create function that plays 1 round of RPS.
+function playGame (playerSelection, computerSelection){
+    const tester = playerSelection + computerSelection;
+        if (playerSelection == computerSelection){
+        console.log("You tied");
+        return "You tied";
+        
+    } else {
+        switch (tester){
+            case "rockpaper":
+                console.log("You lost becuase " + computerSelection + " beats " + playerSelection);
+                return "You lost becuase " + computerSelection + " beats " + playerSelection;
+                break;
+            case "scissorsrock":
+                console.log("You lost becuase " + computerSelection + " beats " + playerSelection);
+                return "You lost becuase " + computerSelection + " beats " + playerSelection;
+                break;
+            case "paperscissors":
+                console.log("You lost becuase " + computerSelection + " beats " + playerSelection);
+                return "You lost becuase " + computerSelection + " beats " + playerSelection;
+                break;
+            case "scissorspaper":
+                console.log("You won becuase " + playerSelection + " beats " + computerSelection);
+                return "You won becuase " + playerSelection + " beats " + computerSelection;
+                break;
+            case "paperrock":
+                console.log("You won becuase " + playerSelection + " beats " + computerSelection);
+                return "You won becuase " + playerSelection + " beats " + computerSelection;
+                break;
+            case "rockscissors":
+                console.log("You won becuase " + playerSelection + " beats " + computerSelection);
+                return "You won becuase " + playerSelection + " beats " + computerSelection;
+                break;
+            default:
+                console.log("something bad happened");
+                break;
+        }
+    }
+}
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+console.log(playGame(playerSelection, computerSelection));
