ff2359996ef99ba03bcb7608691aee609d0a8af9
StaygoldenBoy <staygoldenboy@protonmail.com>
2022-10-07 22:03:56 -0300

    Fix bug with looping of play again and reformat to look cleaner

--- core.js
+++ core.js
@@ -1,3 +1,6 @@
+//Start Game
+console.log(game());
+
 function getComputerChoice(){
     let computerChoice = ['Rock', 'Scissors', 'Paper'];
     let randomSelect = computerChoice[Math.floor((Math.random()) * computerChoice.length)];
@@ -6,9 +9,9 @@ function getComputerChoice(){
     return randomSelect;
     }
 
-    //creating test funciton for random playerselection
-    //created solely to speed up the process of testing all outcomes.
-//function getPlayerChoice(){
+//creating test function for random playerselection
+//created solely to speed up the process of testing all outcomes.
+    //function getPlayerChoice(){
     //let computerChoice = ['Rock', 'Scissors', 'Paper'];
     //let randomPlayerSelect = computerChoice[Math.floor((Math.random()) * computerChoice.length)];
     //randomPlayerSelect = randomPlayerSelect.toLowerCase();
@@ -16,7 +19,7 @@ function getComputerChoice(){
    // return randomPlayerSelect;
    //}
 
-    // Time to create function that plays 1 round of RPS.
+// 1 round of RPS.
 function playGame (playerSelection, computerSelection){
     const tester = playerSelection + computerSelection;
     let result;
@@ -31,7 +34,6 @@ function playGame (playerSelection, computerSelection){
                     case "paperscissors":
                         result = "You lost becuase " + computerSelection + " beats " + playerSelection + ".";
                         break;
-                        
                     case "scissorspaper":
                     case "paperrock":
                     case "rockscissors":
@@ -46,12 +48,13 @@ function playGame (playerSelection, computerSelection){
     } 
 }
 
-
 function game(){
+    console.log("\n\n\n\n\n\n\n\n\n\n"); //Adding space so you can see behind propmt
+    
     let tiescore = 0;
     let playerscore = 0;
     let computerscore = 0;
-
+    
     for (let i = 0; i < 5; i++) {
     let playerPrompt = window.prompt("Choose: Rock, Paper, or Scissors" , ":)");
     playerSelection = playerPrompt.toLowerCase();
@@ -60,19 +63,25 @@ function game(){
     playGame(playerSelection, computerSelection);
     console.log(playGame(playerSelection, computerSelection));
     console.log("");
+
+    //adjust score
     if (playGame(playerSelection, computerSelection).charAt(4) == "t" ){ tiescore = tiescore +1;}
     else if (playGame(playerSelection, computerSelection).charAt(4) == "w") { playerscore = playerscore + 1;}
-    else (playGame(playerSelection, computerSelection).charAt(4) == "l"); {computerscore = computerscore + 1;}     }
+    else {computerscore = computerscore + 1;}     }// end For
 
+    
+    //summarize results
     const finalScore = ( playerscore > computerscore ) ? "Player Wins!"
                     : ( computerscore > playerscore) ? "Computer Wins!"
                     : "Ultimate tie!";
-        return "Final Score:\nWins:" + playerscore + " Losses:" + computerscore + " Ties:" + tiescore + "\n" + finalScore;
-   
+                    
+    console.log("Final Score:\nWins:" + playerscore + " Losses:" + computerscore + " Ties:" + tiescore + "\n" + finalScore);
+        
+    playAgain = window.prompt("Look at your results. Do you want to play again?", "Yes");
+    if (playAgain == "Yes"){
+        game();
+    } else { 
+        window.alert("Tchau");}
+    return "\nThanks for playing!\n\nCoded by: StayGoldenBoy\nGame By: Someone very old";
+    
 }
-
-
-console.log(game());
-
-playAgain = window.prompt("Look at your results. Do you want to play again?", "Yes");
-const doItAgain = (playAgain == "Yes") ? game() : window.alert("Tchau");
